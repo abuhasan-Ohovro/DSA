469f4454a336e755d72436d284de795778bab7df
abuhasan-Ohovro <abuhasanmondal24@gmail.com>
2024-03-07 09:37:06 +0600

    basic discussion

---
+++ basic/basic_01.cpp
@@ -0,0 +1,15 @@
+// In this lecture we will discuss about some basic concept of C++
+//******************************************************************/
+/* Progeam hello world*/
+#include<iostream>  // This is called header file where some bulid in function already written. By inlcuding header file we have acsess to those function.
+using namespace std ; // its used for standard input and output
+int main()// It's called the main funtion. All the program execute in main function.
+{
+  cout<<"Hello Bangladesh!"<<endl;
+  /*Let's analyze this simple code
+   cout: cout is a bulid in function or method which used for output.
+   << : this should be included after cout otherwise It will show an error. You can also called it a C++ lan syntax.
+   endl: It is basically use to for newline. If we use endl after any statement it will move the curser to the next line automatically.
+   */
+  return 0;
+} 
---
+++ basic/basic_02.cpp
@@ -0,0 +1,69 @@
+// Here we will discuss about datatypes in C++
+/*
+There are three type of data type in C++. These are: 
+         1.Primitive data types
+
+               *integer
+               *character
+               *boolean 
+               *floating point
+               *Double floating point
+               *valueless or Void
+               *Wide Character
+         2.Derived Data Types
+               *Function
+               *Array
+               *Pointer
+               *Reference
+         3.Abstract or User-Defined Data types
+               *Class
+               *Structure
+               *Union
+               *Enumeration
+               *Typedef defined Datatype
+*/
+//***********************************************************************/
+//*Sample progeam using primitive data type //
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 234;  //Integer type variable
+    cout<< a << endl;
+
+    char b = 'A'; //Character type variable.Charater type variable shoud be in a single quotation.
+    cout << b << endl;
+
+    bool w = true ; // Boolean type variable.Boolean type variable have two property true and false.true indicate 1 and false indicate 0.
+    cout << w << endl;
+
+    float f = 24.46; // Floating type variable.In floating type variable there can be some point after the decimen point.
+    cout << f << endl;
+
+    double d = 2524524.555;// Double type variable nothing but entented type of float and integer type variable.whrer you can store more larger value.
+    cout << d << endl;
+   
+
+//*************************************************************************************/
+//***************************************Sizeof() method***************
+//* Sizeof() method is used for finding the byte size of a particuler variable.
+//* Sample program of sizeof() method
+ 
+ double dx = 35654235.433;
+ cout<<"Size of the double datatype is-"<< sizeof(dx)<<" Byte "<< endl;// Output : Size of the double datatype is-8 Byte 
+ //*  interger to Character convertion
+  char character_value = 97;
+  cout<< character_value << endl; // Output : a
+  //* Note : By default all the variable can store negative and positive value. But if we want to store only positive nunber we can use (unsigned).
+  unsigned int x  = 23424;
+  cout << x << endl; 
+
+
+
+
+
+
+
+
+
+}
